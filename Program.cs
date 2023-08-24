5f145133581428048f16c7bcf31566cd4adc8d14
IkarosSQ <federleyj@gmail.com>
2023-08-24 12:00:38 +0200

    a

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace vs // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("You are an idiot HAHAHA! You are an idiot HAHAHA! You are an idiot HAHAHA! You are an idiot HAHAHA! You are an idiot HAHAHA!... do you admit that you are an idiot? Y/N - dedsec");
+            Console.WriteLine("You are an idiot HAHAHA! You are an idiot HAHAHA! You are an idiot HAHAHA! You are an idiot HAHAHA!... do you admit that you are an idiot?                                               Y/N                                                     - dedsec");
             if (Console.ReadKey().Key == ConsoleKey.Y)
             {
              Console.Beep(523, 500);
