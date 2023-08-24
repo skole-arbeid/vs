8443ebf9649ab1f53e6be87489f37ba93337d4b8
IkarosSQ <federleyj@gmail.com>
2023-08-24 09:29:17 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using System.Collections;
+
+namespace vs // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("du har klamidia");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+    }
+}
