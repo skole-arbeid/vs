0ea08c4eb58ba76108d65679dc1e4ccca566e0b2
IkarosSQ <federleyj@gmail.com>
2023-08-28 09:39:52 +0200

    a

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Dynamic;
 using System.Runtime.CompilerServices;
 using System.Collections;
+using System.Reflection.Metadata.Ecma335;
 
 namespace vs // Note: actual namespace depends on the project name.
 {
@@ -9,7 +10,12 @@ namespace vs // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            Console.Title = "arne";
             Console.ForegroundColor = ConsoleColor.DarkGreen;
+            
+            Console.WindowHeight = 40;
+            
+            
             Console.WriteLine("You are an idiot HAHAHA! You are an idiot HAHAHA! You are an idiot HAHAHA! You are an idiot HAHAHA!...                  do you admit that you are an idiot?                                                                                     Y/N");
             if (Console.ReadKey().Key == ConsoleKey.Y)
             {
