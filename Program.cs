e334ecd89227e566d5b9570a389cbca27c125062
IkarosSQ <federleyj@gmail.com>
2023-09-06 11:33:12 +0200

    a

--- Program.cs
+++ Program.cs
@@ -26,12 +26,12 @@ namespace vs // Note: actual namespace depends on the project name.
             Console.WriteLine("choose 1 or 2");
             
             if (Console.ReadKey().Key == ConsoleKey.NumPad1) {
-                Console.WriteLine("you chose alien #1");
-                Console.Write(meetAlien2);
+                Console.WriteLine("\nyou chose alien #1");
+                meetAlien1();
                 Console.WriteLine("have a date?");
             } else if (Console.ReadKey().Key == ConsoleKey.NumPad2) {
-                Console.WriteLine("you chose alien #2");
-                Console.Write(meetAlien2);
+                Console.WriteLine("\nyou chose alien #2");
+                meetAlien2();
                 Console.WriteLine("have a date?");
             }
 
